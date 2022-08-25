bc5cd42d1622d7713a18808b831896d23c8e41f0
Anmol Bansal <anmolbansal1807@gmail.com>
2022-08-25 11:10:30 +0530

    register page

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,12 +1,14 @@
 import React,{useState} from "react";
 import {useNavigate} from 'react-router-dom';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye ,faEyeSlash } from "@fortawesome/free-regular-svg-icons";
 import LoginNav from "./LoginNav";
 import cylinder from "../assets/cylinder.png"
 import gear from "../assets/gear.png"
 
 export default function Login(){
     const navigate = useNavigate();
-    const [credentials, setCredentials] = useState({username:"",password:""});
+    const [credentials, setCredentials] = useState({username:"",password:"", isVisible: false});
 
    function handleClick(input) { 
 setCredentials((prevCred)=>({...prevCred, [input.target.name]: input.target.value}))
@@ -14,6 +16,9 @@ setCredentials((prevCred)=>({...prevCred, [input.target.name]: input.target.valu
 }
     function handleSubmit(e){
         e.preventDefault();
+    }
+    function toggleView(){
+        setCredentials((prevCred)=>({...prevCred, isVisible: !prevCred.isVisible}));
     }
     return(
         <div className="login-page">
@@ -23,7 +28,8 @@ setCredentials((prevCred)=>({...prevCred, [input.target.name]: input.target.valu
         <h1>Log in with</h1>
         <div className='login-container'>
         <input type="text" value={credentials.username} onChange={handleClick} name="username" placeholder="Username" className="login-input" />
-            <input type="password" onChange={handleClick} value={credentials.password} name="password" placeholder="Password" className="login-input" />
+            <input type={credentials.isVisible ? "text": "password"} onChange={handleClick} value={credentials.password} name="password" placeholder="Password" className="login-input" />
+            <FontAwesomeIcon className="icon" icon={credentials.isVisible ? faEye : faEyeSlash} onClick={toggleView} />
         </div>
         <div className="btn-container">
         <button onClick={handleSubmit} className="login-btn">Login</button>
