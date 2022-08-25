080a35afedacd580e166ec816d1f4edd8aab000c
Anmol Bansal <anmolbansal1807@gmail.com>
2022-08-25 23:42:30 +0530

    register page

--- src/components/svgSlingy.js
+++ src/components/svgSlingy.js
@@ -1,13 +1,11 @@
 import React from "react";
-import {motion, useScroll, useSpring} from 'framer-motion'
+import {motion} from 'framer-motion'
 
 
 export default function SvgSlingy(){
-    const {scrollYProgress} = useScroll();
-    const YValue = useSpring(scrollYProgress);
-    console.log(YValue);
+    
     return(
-        <motion.div initial={{x: 0, y: 0}} animate={{x: YValue,y : YValue}} transition={{duration: 2}}>
+        <motion.div animate={{x:-80,y:30}} transition={{type:"tween", duration: 4}}>
         <svg width="266" className='slingy' height="216" viewBox="0 0 266 216" fill="none" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
 <rect width="258.925" height="88.6113" transform="matrix(-0.839587 0.543225 0.543225 0.839587 217.444 0.266113)" fill="url(#pattern4)"/>
 <defs>
